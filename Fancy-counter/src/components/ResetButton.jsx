6c0b6419b61dbfe175845192ac48cfc54324e94e
YBoah <yawboahene.yb@gmail.com>
2024-05-14 08:28:14 +0000

    Restructing the resetbutton

--- Fancy-counter/src/components/ResetButton.jsx
+++ Fancy-counter/src/components/ResetButton.jsx
@@ -1,10 +1,16 @@
 import { ResetIcon } from "@radix-ui/react-icons";
+
 function ResetButton({ setCount }) {
+  
+const handleClick = () => {
+  () => {
+    setCount(0);
+  };
+};
+
   return (
     <button
-      onClick={() => {
-        setCount(0);
-      }}
+      onClick={handleClick}
       className="reset-btn"
     >
       <ResetIcon className="reset-btn-icon" />
